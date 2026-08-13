cd945f5a7a12b6a1f02b90d6c01549ad84790d43
Henry R <henryr@Henrys-Mac-mini.local>
2026-08-13 04:15:11 -0700

    Fix: typo

--- Components/Pages/Jets.tsx
+++ Components/Pages/Jets.tsx
@@ -7,7 +7,7 @@ const header = "Worked on one of these.";
 const description = [
   "APG79/APG82. F-15 Eagle, F/A-18 Hornet.",
   "Developed software for APG79/APG82 Active Electronically Scanned Array Radar. Delivered builds for U.S, Japan, Kuwait, and Qatar radar systems.",
-  "Mode development including Air-to-Air, Sea Surface Search, GTMI (Ground Moving Target Indicator), Air-to-Ground, SAR Maps (Synthetic Aperture Radar), Attack modes, and more modes.",
+  "Mode development including Air-to-Air, Sea Surface Search, GMTI (Ground Moving Target Indicator), Air-to-Ground, SAR Maps (Synthetic Aperture Radar), Attack modes, and more modes.",
 ];
 
 const Jets = () => {
